ea31de727cb992c0e8829f56705b53d7cb836eef
serhei_mikhailov <serheimikhailovjob@gmail.com>
2020-10-24 02:14:10 +0300

    button show is ok

--- js/create_table.js
+++ js/create_table.js
@@ -1,73 +1,4 @@
-// create a new array
 
-let allTasksRadio = document.querySelectorAll(`.all_tasks_radio`);
-let allTasksRadioCheckedIndex;
-
-for (let i = 0; i < allTasksRadio.length; i++) {
-    if (allTasksRadio[i].checked) {
-        allTasksRadioCheckedIndex = i;
-    }
-}
-
-let allUrgentRadio = document.querySelectorAll(`.all_urgency_radio`);
-let allUrgentRadioCheckedIndex;
-
-for (let i = 0; i < allUrgentRadio.length; i++) {
-    if (allUrgentRadio[i].checked) {
-        allUrgentRadioCheckedIndex = i;
-    }
-}
-
-// function for new array
-
-let arrAfterTasksChecked = [];
-
-for (let i = 0; i < initialData.length; i++) {
-    if (allTasksRadioCheckedIndex === 0) {
-        arrAfterTasksChecked = initialData;
-    }
-    if (allTasksRadioCheckedIndex === 1) {
-        if (initialData[i].completed === true) {
-            arrAfterTasksChecked.push(initialData[i]);
-        }
-    }
-    if (allTasksRadioCheckedIndex === 2) {
-        if (initialData[i].deleted === true) {
-            arrAfterTasksChecked.push(initialData[i]);
-        }
-    }
-}
-
-console.log (arrAfterTasksChecked);
-
-
-
-let arrAfterUrgencyChecked = [];
-
-for(let i = 0; i < arrAfterTasksChecked.length; i++) {
-    if (allUrgentRadioCheckedIndex === 0) {
-        arrAfterUrgencyChecked = arrAfterTasksChecked;
-    }
-    if (allUrgentRadioCheckedIndex === 1) {
-        if (arrAfterTasksChecked[i].urgency === 0) {
-            arrAfterUrgencyChecked.push(arrAfterTasksChecked[i]);
-        }
-    }
-    if (allUrgentRadioCheckedIndex === 2) {
-        if (arrAfterTasksChecked[i].urgency === 1) {
-            arrAfterUrgencyChecked.push(arrAfterTasksChecked[i]);
-        }
-    }
-    if (allUrgentRadioCheckedIndex === 3) {
-        if (arrAfterTasksChecked[i].urgency === 2) {
-            arrAfterUrgencyChecked.push(arrAfterTasksChecked[i]);
-        }
-    }
-}
-
-
-
-console.log (arrAfterUrgencyChecked);
 
 //////////////////////////////
 
@@ -84,7 +15,76 @@ const createTable = function () {
 
 
 
-    
+        // create a new array
+
+    let allTasksRadio = document.querySelectorAll(`.all_tasks_radio`);
+    let allTasksRadioCheckedIndex;
+
+    for (let i = 0; i < allTasksRadio.length; i++) {
+        if (allTasksRadio[i].checked) {
+            allTasksRadioCheckedIndex = i;
+        }
+    }
+
+    let allUrgentRadio = document.querySelectorAll(`.all_urgency_radio`);
+    let allUrgentRadioCheckedIndex;
+
+    for (let i = 0; i < allUrgentRadio.length; i++) {
+        if (allUrgentRadio[i].checked) {
+            allUrgentRadioCheckedIndex = i;
+        }
+    }
+
+    // function for new array
+
+    let arrAfterTasksChecked = [];
+
+    for (let i = 0; i < initialData.length; i++) {
+        if (allTasksRadioCheckedIndex === 0) {
+            arrAfterTasksChecked = initialData;
+        }
+        if (allTasksRadioCheckedIndex === 1) {
+            if (initialData[i].completed === true) {
+                arrAfterTasksChecked.push(initialData[i]);
+            }
+        }
+        if (allTasksRadioCheckedIndex === 2) {
+            if (initialData[i].deleted === true) {
+                arrAfterTasksChecked.push(initialData[i]);
+            }
+        }
+    }
+
+    console.log (arrAfterTasksChecked);
+
+
+
+    let arrAfterUrgencyChecked = [];
+
+    for(let i = 0; i < arrAfterTasksChecked.length; i++) {
+        if (allUrgentRadioCheckedIndex === 0) {
+            arrAfterUrgencyChecked = arrAfterTasksChecked;
+        }
+        if (allUrgentRadioCheckedIndex === 1) {
+            if (arrAfterTasksChecked[i].urgency === 0) {
+                arrAfterUrgencyChecked.push(arrAfterTasksChecked[i]);
+            }
+        }
+        if (allUrgentRadioCheckedIndex === 2) {
+            if (arrAfterTasksChecked[i].urgency === 1) {
+                arrAfterUrgencyChecked.push(arrAfterTasksChecked[i]);
+            }
+        }
+        if (allUrgentRadioCheckedIndex === 3) {
+            if (arrAfterTasksChecked[i].urgency === 2) {
+                arrAfterUrgencyChecked.push(arrAfterTasksChecked[i]);
+            }
+        }
+    }
+
+
+
+    console.log (arrAfterUrgencyChecked);
 
 
 
@@ -95,17 +95,17 @@ const createTable = function () {
     const tableTag = document.createElement('table');
     mainTable.append(tableTag);
 
-    for (let i = 0; i < initialData.length; i++) {
+    for (let i = 0; i < arrAfterUrgencyChecked.length; i++) {
         let trTag = document.createElement(`tr`);
         tableTag.prepend(trTag);
 
         // URGENCY
 
         let tdTagUrgency = document.createElement(`td`);
-        if (initialData[i].urgency === 0) {
+        if (arrAfterUrgencyChecked[i].urgency === 0) {
             tdTagUrgency.classList.add(`td_urgency`, `td_urgent`, 'td_center');
             tdTagUrgency.innerText = `urgent`;
-        } else if (initialData[i].urgency === 1) {
+        } else if (arrAfterUrgencyChecked[i].urgency === 1) {
             tdTagUrgency.classList.add(`td_urgency`, `td_not_urgent`, 'td_center');
             tdTagUrgency.innerText = `not urgent`;
         } else {
@@ -119,13 +119,13 @@ const createTable = function () {
 
         let tdTagName = document.createElement(`td`);
         tdTagName.classList.add(`td_name`);
-        tdTagName.innerText = `${initialData[i].name}`;
+        tdTagName.innerText = `${arrAfterUrgencyChecked[i].name}`;
         trTag.append(tdTagName);
 
         // COMPLETED
 
         let tdTagCompleted = document.createElement(`td`);
-        if (initialData[i].completed === true) {
+        if (arrAfterUrgencyChecked[i].completed === true) {
             tdTagCompleted.classList.add(`td_completed`, `green`, 'td_center');
 
         } else {
@@ -139,7 +139,7 @@ const createTable = function () {
 
         let tdTagDeleted = document.createElement(`td`);
 
-        if (initialData[i].deleted === true) {
+        if (arrAfterUrgencyChecked[i].deleted === true) {
             tdTagDeleted.classList.add(`td_deleted`, `red`, 'td_center');
 
         } else {
@@ -152,7 +152,7 @@ const createTable = function () {
         // DESCRIPTION
 
         let tdTagDescription = document.createElement(`td`);
-        if (initialData[i].description) {
+        if (arrAfterUrgencyChecked[i].description) {
             tdTagDescription.classList.add(`black`, 'td_center');
             tdTagDescription.innerText = `description`;
             trTag.append(tdTagDescription);
